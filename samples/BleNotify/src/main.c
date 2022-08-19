45a8d0b1edaec422b1414a57e60a9ef801c08e63
Damian Suess <djsuess@gmail.com>
2022-08-19 14:19:54 -0400

    Moved samples

---
+++ samples/BleNotify/src/main.c
@@ -0,0 +1,11 @@
+#include <freertos/FreeRTOS.h>   // Include the base FreeRTOS definitions
+#include <freertos/task.h>       // Include the task definitions
+
+#include "..\..\..\src\BLEDevice.h"
+#include "..\..\..\src\BLEServer.h"
+#include "..\..\..\src\BLEUtils.h"
+
+void app_main()
+{
+  //
+}
